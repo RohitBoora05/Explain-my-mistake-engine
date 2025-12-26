6c0e5330fde29a98d9279354b7f9c18ab477308f
Rohit <you@example.com>
2025-12-26 18:45:21 +0530

    created content file to store values that  we gona infier in futuer when taking decison

---
+++ core/costent.py
@@ -0,0 +1,21 @@
+# if answered before 25 sec
+IMPULSE_TIME_THRESHOLD = 25.0
+
+# took to much time more than 2 min.
+OVERTHINK_TIME_THRESHOLD = 120.0
+
+#confidence level
+LOW_CONFIDENCE_THRESHOLD = 0.35
+HIGH_CONFIDENCE_THRESHOLD = 0.75
+
+#value for mistake 
+PRIMARY_MISTAKE_SCORE_THRESHOLD = 0.60
+
+# value difference in mistakes
+DOMINANCE_MARGIN = 0.15
+
+#eliminated option 
+MIN_EFFECTIVE_ELIMINATIONS = 2
+
+#too much change 
+MULTIPLE_OPTION_CHANGES = 2
